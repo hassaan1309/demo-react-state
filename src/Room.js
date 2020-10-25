f76bbc436e3fb0e180688723e0a30b4affc3f57a
hassaan <hassaanahmad1309@gmail.com>
2020-10-25 16:07:52 +0500

    created state react demo app

---
+++ src/Room.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function Room() {
+
+    function ToggleLight(){
+        console.log("Toggle Light Button clicked.");
+        setLit(!isLit);
+    }
+
+    function UpdateAge(){
+        console.log("Update Age Button clicked.");
+        setAge(++Age);
+    }
+
+    //const state = useState(true);
+    //console.log("State = ",state);
+    let [isLit,setLit] = useState(false);
+    let [Age, setAge] = useState(20);
+  return (
+    <div>
+     This room is {isLit ? 'lit' : 'dark'}.
+     <br/>
+     My age is {Age} years.
+     <br/>
+     <button onClick={ToggleLight}>Toggle Light</button>
+     <button onClick={UpdateAge}>Update Age</button>
+    </div>
+  );
+}
+
+export default Room;
