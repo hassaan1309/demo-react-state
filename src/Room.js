4dea8e74bad363f86b305e77520088e7f276356d
hassaan <hassaanahmad1309@gmail.com>
2020-10-26 00:39:45 +0500

    modified room.js

--- src/Room.js
+++ src/Room.js
@@ -13,10 +13,29 @@ function Room() {
         setAge(++Age);
     }
 
+    function setLightOn(){
+      console.log("Light On button clicked.");
+      setLit(true);
+    }
+
+    function setLightOff(){
+      console.log("Light Off button clicked.");
+      setLit(false);
+    }
+
+    function incTemperature(){
+      setTemperature(++Temperature);
+    }
+
+    function decTemperature(){
+      setTemperature(--Temperature);
+    }
+
     //const state = useState(true);
     //console.log("State = ",state);
     let [isLit,setLit] = useState(false);
     let [Age, setAge] = useState(20);
+    let [Temperature, setTemperature] = useState(22);
   return (
     <div className = {`room ${isLit ? "lit" : "dark"}`}>
      This room is {isLit ? 'lit' : 'dark'}.
@@ -25,6 +44,18 @@ function Room() {
      <br/>
      <button onClick={ToggleLight}>Toggle Light</button>
      <button onClick={UpdateAge}>Update Age</button>
+     <br/>
+     This is the code to turn the light on and off.
+     <br/>
+     The light is now {isLit ? 'on' : 'off'}.
+     <br/>
+     <button onClick={setLightOn}>On</button>
+     <button onClick={setLightOff}>Off</button>
+     <br/>
+     The current temperature is {Temperature} celcius.
+     <br/>
+     <button onClick={() => setTemperature(++Temperature)}>+</button>
+     <button onClick={decTemperature}>-</button>
     </div>
   );
 }
